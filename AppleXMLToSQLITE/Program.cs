7d5530bea45be7e97622afd8b19c8cc6c6d17878
Sean Hagen <sean.r.hagen@gmail.com>
2021-06-19 14:07:19 -0500

    Add project files.

---
+++ AppleXMLToSQLITE/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+using Serilog;
+using Serilog.Core;
+using Serilog.Enrichers;
+
+namespace AppleXMLToSQLITE
+{
+    class Program
+    {
+        private static Logger Logger = new LoggerConfiguration().Enrich.With(new ThreadIdEnricher()).WriteTo.Console(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}", restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Error).MinimumLevel.Error().CreateLogger();
+        private static Dictionary<string, int> tableKeyCount = new Dictionary<string, int>();
+        static void Main(string[] args)
+        {
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            XmlReader reader = XmlReader.Create(@".\..\..\export\apple_health_export\export.xml", settings);
+            reader.MoveToContent();
+
+            Utilities util = new Utilities("health.db");
+            List<Tuple<string, string>> attributeList = new List<Tuple<string, string>>();
+            string nodeName = "";
+            bool inrecord = false;
+            // Parse the file and display each of the nodes.
+            while (reader.Read())
+            {
+                try
+                {
+                    // Single Element
+                    if (reader.IsEmptyElement && reader.Name != "MetadataEntry" && !inrecord)
+                    {
+                        nodeName = reader.Name;
+                        int i = 0;
+                        if (nodeName == "Record")
+                        {
+                            reader.MoveToAttribute(0);
+                            i++;
+                            nodeName = reader.Value.Replace("HKQuantityTypeIdentifier", "");
+                        }
+                        for (; i < reader.AttributeCount; i++)
+                        {
+                            reader.MoveToAttribute(i);
+                            attributeList.Add(new Tuple<string, string>(reader.Name, reader.GetAttribute(i).Replace("HKQuantityTypeIdentifier", "")));
+                        }
+                        if (attributeList.Count > 0)
+                        {
+                            util.enterRecord(attributeList, nodeName);
+                            attributeList = new List<Tuple<string, string>>();
+                            updateDict(nodeName);
+                        }
+                    }
+                    // Enter big record
+                    else if (reader.NodeType != XmlNodeType.EndElement && !inrecord && reader.NodeType != XmlNodeType.Whitespace)
+                    {
+                        int i = 0;
+                        inrecord = true;
+                        reader.MoveToAttribute(0);
+                        i++;
+                        nodeName = reader.Value.Replace("HKQuantityTypeIdentifier", "");
+
+                        for (; i < reader.AttributeCount; i++)
+                        {
+                            reader.MoveToAttribute(i);
+                            attributeList.Add(new Tuple<string, string>(reader.Name, reader.GetAttribute(i).Replace("HKQuantityTypeIdentifier", "")));
+                        }
+                        if (attributeList.Count > 0)
+                        {
+                            util.enterRecord(attributeList, nodeName);
+                            attributeList = new List<Tuple<string, string>>();
+                            updateDict(nodeName);
+                        }
+                    }
+                    //Enter meta data
+                    else if(reader.NodeType != XmlNodeType.EndElement && reader.NodeType != XmlNodeType.Whitespace)
+                    {
+                        string metaDataTableName = reader.Name;
+                        for (int i = 0; i < reader.AttributeCount; i++)
+                        {
+                            reader.MoveToAttribute(i);
+                            attributeList.Add(new Tuple<string, string>(reader.Name, reader.GetAttribute(i).Replace("HKQuantityTypeIdentifier", "")));
+                        }
+                        if (attributeList.Count > 0)
+                        {
+                            util.enterMeta(attributeList, tableKeyCount.GetValueOrDefault(nodeName), nodeName, metaDataTableName);
+                            attributeList = new List<Tuple<string, string>>();
+                        }
+                    }
+                    else
+                    {
+                        inrecord = false;
+                        if (attributeList.Count > 0)
+                        {
+                            util.enterRecord(attributeList, nodeName);
+                            attributeList = new List<Tuple<string, string>>();
+                            updateDict(nodeName);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e.Message);
+                    inrecord = false;
+                    attributeList = new List<Tuple<string, string>>();
+                }
+
+
+            }
+
+        }
+
+        private static void updateDict(string tableName)
+        {
+
+            if (tableKeyCount.ContainsKey(tableName))
+            {
+                int count = 0;
+                tableKeyCount.Remove(tableName, out count);
+                tableKeyCount.Add(tableName, ++count);
+            }
+            else
+            {
+                tableKeyCount.Add(tableName, 0);
+            }
+
+        }
+    }
+}
---
+++ AppleXMLToSQLITE/Utilities.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Data.SQLite;
+using System.Collections;
+using Serilog.Core;
+using Serilog;
+using Serilog.Enrichers;
+
+namespace AppleXMLToSQLITE
+{
+    class Utilities
+    {
+        private SQLiteConnection sqlConn;
+        private List<string> tableList;
+        private static Logger Logger = new LoggerConfiguration().Enrich.With(new ThreadIdEnricher()).WriteTo.Console(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}").WriteTo.File("./logs/log").CreateLogger();
+
+        public Utilities(string filePath)
+        {
+            if (!File.Exists("health.db"))
+            {
+                SQLiteConnection.CreateFile("health.db");
+            }
+            this.sqlConn = new SQLiteConnection("Data Source=health.db;Version=3;");
+            this.sqlConn.Open();
+            this.tableList = new List<string>();
+
+        }
+
+        //private bool checkIfTableExists(string table)
+        //{
+        //    SQLiteCommand com = this.sqlConn.CreateCommand();
+        //    com.CommandText = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name=@tableName;";
+        //    com.Parameters.AddWithValue("@tableName", table);
+        //    int count = Convert.ToInt32(com.ExecuteScalar());
+        //    return count > 0 ? true : false;
+
+        //}
+
+        private bool createTable(List<Tuple<string, string>> attributes, string table, bool meta)
+        {
+            SQLiteCommand com = this.sqlConn.CreateCommand();
+            string qry = "CREATE TABLE " + table + " ( ";
+            if (meta)
+            {
+                qry += " FK INT, tableReference TEXT,";
+            }
+
+            for (int i = 0; i < attributes.Count; i++)
+            {
+                Tuple<string, string> tup = attributes[i];
+                if (i != attributes.Count - 1)
+                {
+                    if (tup.Item1.ToLower().Contains("date"))
+                    {
+                        qry += tup.Item1 + " DATETIME, ";
+                    }
+                    else if (tup.Item1.ToLower().Contains("value"))
+                    {
+                        qry += tup.Item1 + " INT, ";
+                    }
+                    else
+                    {
+                        qry += tup.Item1 + " TEXT, ";
+                    }
+                }
+                else
+                {
+                    if (tup.Item1.ToLower().Contains("date"))
+                    {
+                        qry += tup.Item1 + " DATETIME ";
+                    }
+                    else if (tup.Item1.ToLower().Contains("value"))
+                    {
+                        qry += tup.Item1 + " INT ";
+                    }
+                    else
+                    {
+                        qry += tup.Item1 + " TEXT ";
+                    }
+                }
+            }
+            qry += " );";
+            com.CommandText = qry;
+            Logger.Information(String.Format("Created table {0}", table));
+            int count = com.ExecuteNonQuery();
+            this.tableList.Add(table);
+            return count > 0 ? true : false;
+
+        }
+
+        private List<string> getCollumns(string table)
+        {
+            List<string> list = new List<string>();
+            SQLiteCommand com = this.sqlConn.CreateCommand();
+            string qry = string.Format("pragma table_info({0});", table);
+            com.CommandText = qry;
+            SQLiteDataReader res = com.ExecuteReader();
+            while (res.Read())
+            {
+                list.Add(res.GetString(1));
+            }
+            return list;
+        }
+
+        private bool addColumn(Tuple<string,string> tup, string table)
+        {
+            SQLiteCommand com = this.sqlConn.CreateCommand();
+            string qry = "ALTER " + table + " add column ";
+            if (tup.Item1.ToLower().Contains("date"))
+            {
+                qry += tup.Item1 + " DATETIME ";
+            }
+            else if (tup.Item1.ToLower().Contains("value"))
+            {
+                qry += tup.Item1 + " INT ";
+            }
+            else
+            {
+                qry += tup.Item1 + " TEXT ";
+            }
+            com.CommandText = qry;
+            int count = com.ExecuteNonQuery();
+            Logger.Information(String.Format("Added collumn {0} to {1}", tup.Item1, table));
+            return count > 0 ? true : false;
+        }
+
+
+        private bool updateColumnList(List<Tuple<string, string>> attributes, string table)
+        {
+            List<string> list = getCollumns(table);
+
+            SQLiteCommand com = this.sqlConn.CreateCommand();
+            for (int i = 0; i < attributes.Count; i++)
+            {
+                Tuple<string, string> tup = attributes[i];
+                if (!list.Contains(tup.Item1))
+                {
+                    addColumn(tup, table);
+                }
+            }
+            int count = com.ExecuteNonQuery();
+            return count > 0 ? true : false;
+
+        }
+
+        public bool enterRecord(List<Tuple<string, string>> attributes, string tableName)
+        {
+            if (!this.tableList.Contains(tableName))
+            {
+                createTable(attributes, tableName, false);
+            }
+            SQLiteCommand com = this.sqlConn.CreateCommand();
+            string qry = "INSERT INTO " + tableName + "( ";
+            string vals = " ( ";
+            for(int i = 0; i < attributes.Count; i++)
+            {
+                Tuple<string, string> tup = attributes[i];
+                if(i != attributes.Count - 1)
+                {
+                    qry += tup.Item1 + ", ";
+                    vals += "@" + tup.Item1 + ", ";
+                    com.Parameters.AddWithValue("@" + tup.Item1, tup.Item2);
+                }
+                else
+                {
+                    qry += tup.Item1;
+                    vals += "@" + tup.Item1;
+                    com.Parameters.AddWithValue("@" + tup.Item1, tup.Item2);
+                }
+
+            }
+            qry += " ) VALUES " + vals + " )";
+            com.CommandText = qry;
+            int count = 0;
+            try
+            {
+                count = com.ExecuteNonQuery();
+                //Logger.Information(String.Format("Entered Record into {0}", tableName));
+            }catch(Exception e)
+            {
+                updateColumnList(attributes, tableName);
+                com = this.sqlConn.CreateCommand();
+                com.CommandText = qry;
+                count = com.ExecuteNonQuery();
+            }
+
+
+            return count > 0 ? true : false;
+        }
+
+        public bool enterMeta(List<Tuple<string, string>> attributes, int fk, string tableNameReference, string metaTableName)
+        {
+            if (!this.tableList.Contains(metaTableName))
+            {
+                createTable(attributes, metaTableName, true);
+            }
+            SQLiteCommand com = this.sqlConn.CreateCommand();
+            string qry = "INSERT  INTO " + metaTableName + "( FK, tableReference, ";
+            string vals = " ";
+            for (int i = 0; i < attributes.Count; i++)
+            {
+                Tuple<string, string> tup = attributes[i];
+                if (i != attributes.Count - 1)
+                {
+                    qry += tup.Item1 + ", ";
+                    vals += "@" + tup.Item1 + ", ";
+                    com.Parameters.AddWithValue("@" + tup.Item1, tup.Item2);
+                }
+                else
+                {
+                    qry += tup.Item1;
+                    vals += "@" + tup.Item1;
+                    com.Parameters.AddWithValue("@" + tup.Item1, tup.Item2);
+                }
+
+            }
+            qry += " ) VALUES ( " + fk + ", '" + tableNameReference + "', " + vals + " )";
+            com.CommandText = qry;
+            int count = 0;
+            try
+            {
+                count = com.ExecuteNonQuery();
+                //Logger.Information(String.Format("Entered Record into {0}", "MetadataEntry"));
+            }
+            catch (Exception e)
+            {
+                updateColumnList(attributes,metaTableName);
+                com = this.sqlConn.CreateCommand();
+                com.CommandText = qry;
+                count = com.ExecuteNonQuery();
+            }
+
+
+            return count > 0 ? true : false;
+        }
+
+
+    }
+}
